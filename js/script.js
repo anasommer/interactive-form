aacf1350b4ae0c3713bff16d70c53cfa25ddf928
anasommer <anastassia.sommer@gmail.com>
2021-06-13 19:46:44 +0200

    Only accessibility needed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+const formEl = document.querySelector('form');
+const email = document.querySelector('#email');
+const zipCode = document.querySelector('#zip');
+const cvv = document.querySelector('#cvv');
+const cardNumber = document.querySelector('#cc-num');
+
 // 'Name' field is being focused at the page load
 const nameField = document.querySelector('#name');
 nameField.focus();
@@ -7,13 +13,13 @@ const otherJobRole = document.querySelector('#other-job-role');
 otherJobRole.style.visibility = 'hidden';
 
 const jobRole = document.querySelector('#title');
-jobRole.addEventListener('change', (e)=>{
+jobRole.addEventListener('change', e =>{
    if (e.target.value === 'other') {
     otherJobRole.style.visibility = 'visible';
    } else {
     otherJobRole.style.visibility = 'hidden';
    }
-});
+})
 
 // Disable 'Color' select element
 const colorSelect = document.querySelector('#color');
@@ -22,7 +28,7 @@ colorSelect.disabled = true;
 
 // Design element should listen for user changes and show only t-shirt colors available for choosed design
 const designSelect = document.querySelector('#design');
-designSelect.addEventListener('change', (e) => {
+designSelect.addEventListener('change', e => {
     colorSelect.disabled = false;
     for (let i = 1; i < colorOptions.length; i++) {
         const theme = e.target.value;
@@ -37,36 +43,127 @@ designSelect.addEventListener('change', (e) => {
             colorOptions[i].setAttribute('selected', false);
         }
     }
-});
+})
 
 // The Total $ el below the Register for Activities section should update to reflect the sum of the cost of the user's selected activities
+const total = document.querySelector('#activities-cost');
+const activities = document.querySelector('#activities');
+const allActivities = document.querySelectorAll('#activities input');
+let totalCost = 0;
 
-    // Register for Activities fieldset shoul listen for user changes. When a change is detected:
-    // the total cost should increase by the value on the data-cost attr of the activity's input type = 'checkbox' el
-    // if an activity is unchecked, the total cost should decrease by that amount
-    // the <p> el with the id of 'activity-cost' below the activities section should update to reflect the chosen activities total cost
+    activities.addEventListener('change', e => {
+      const clicked = e.target;   
+      const clickedType = clicked.getAttribute('data-day-and-time');
+      const activityCost = +clicked.getAttribute('data-cost');
+     
+      for(let i = 1; i < allActivities.length; i++) {
+        const activityType = allActivities[i].getAttribute('data-day-and-time');
+
+        if (clickedType === activityType && clicked !== allActivities[i]) {
+            clicked.checked ? allActivities[i].disabled = true :
+            allActivities[i].disabled = false;
+        }
+      }
+
+        if (clicked.checked) { 
+            totalCost += activityCost;
+        } else if (!clicked.checked) {
+            totalCost -= activityCost;
+        } 
+      total.innerHTML = `Total: $${totalCost}`;
+    })
+
+
 
 // The credit card payment option should be selected for the user by default. When the form first loads, 'Credit Card' should be displayed in the 'I'm going to pay with' <select> el
-    // Hide other payment options from showing on page load
-    // 'I'm going to pay' <select> el should listen to changes.
-    // When change is detected, hide all payment sections in the form's UI except the selected one.
+const payment = document.querySelector('#payment');
+const paypal = document.querySelector('#paypal');
+const bitcoin = document.querySelector('#bitcoin');
+const creditCard = document.querySelector('#credit-card');
 
-// Users shouldn't be able to submit a form without the required info, or with invalid info.
-//Create your own custom validation
-// form el should listen for the submit event.
-    // when the form submission is detected, each required form field or section should be validated, or checked.
-    //prevent form's submission if required fields is not valid.
+paypal.style.display = 'none';
+bitcoin.style.display = 'none';
+payment.children[1].selected = true;
 
-    // the Name field cannot be blank or empty
-    // Email address field must contain a validly formatted email address. It doesn't need to be real address, just formatted like one. 
-    // the Register for Activities section must have at least one activity selected.
-    // If and only if credit card is the selected payment method:
-        // the Card number field must contain a 13-16 digit credit card number with no dashes or spaces. 
-        // Zip code field must contain 5 digit number.
-        // the CVV field must contain a 3 digit number.
+    payment.addEventListener('change', e => {
+        const paymentMethod = e.target.value;
+        if (paymentMethod === 'paypal') {
+            hidePaymentMethods(paypal, creditCard, bitcoin);
+        } if (paymentMethod === 'bitcoin') {
+            hidePaymentMethods(bitcoin, creditCard, paypal);
+        } if (paymentMethod === 'credit-card') {
+            hidePaymentMethods(creditCard, bitcoin, paypal);
+        }
+    })
+
+    function hidePaymentMethods (show, hideMethod1, hideMethod2) {
+        show.style.display = '';
+        hideMethod1.style.display = 'none';
+        hideMethod2.style.display = 'none';     
+    }
+
+
+formEl.addEventListener('submit', e => {
+    if (!nameValidator()) {
+        e.preventDefault();
+    } else if (!emailValidator()) {
+        e.preventDefault();
+    } else if (!activitiesValidator()) {
+        e.preventDefault();
+    } else if (!validCardNumber()) {
+        e.preventDefault();
+    } else if (!validZip()) {
+        e.preventDefault();
+    } else if (!validCvv()) {
+        e.preventDefault();
+    }
+})
+
+// Form Validators
+const nameValidator = () => {
+    const nameValue = nameField.value;
+    const validName = /^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(nameValue);
+    return validName;
+}
+
+const emailValidator = () => {
+    const emailValue = email.value;
+    const validEmail = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailValue);
+    return validEmail;
+}
+
+const activitiesValidator = () => {
+    const validActivities = totalCost > 0;
+    return validActivities;
+}
+
+const validCardNumber = () => {
+    const cardNumberValue = cardNumber.value;
+    const validCardNumber = /^\d{13,16}$/.test(cardNumberValue);
+    return validCardNumber;
+}
+
+const validZip = () => {
+    const zipValue = zipCode.value;
+    const validZip = /^\d{5}$/.test(zipValue);
+    return validZip;
+} 
+
+const validCvv = () => {
+    const cvvValue = cvv.value;
+    const validCvv = /^\d{3}$/.test(cvvValue);
+    return validCvv;
+}
+
+activities.addEventListener('focus', () => {
+    for (let i = 1; i < allActivities.length; i ++) {
+        console.log(allActivities[i]);
+    }
+})
 
 // Make the focus states of the activities more obvious to all users. 
     // Make all of the activity checkbox input elements to listen for the focus and blur events.
+
         // When the focus event is detected, add the .focus className to the checkbox input's parent label element.
         // When the blur event is detected, remove the .focus className from the label element. It can be helpful here to directly target the element with the className of .focus in order to remove it.
 
